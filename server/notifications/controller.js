aa4cc8c9082b684b113922df4a8b12dcbe638787
Vijay Ram <selvanprograms@gmail.com>
2015-02-24 15:44:38 -0800

    Clean up some controllers

--- server/notifications/controller.js
+++ server/notifications/controller.js
@@ -7,54 +7,79 @@ var controller = {};
 
 controller.create = function(req, res, next){
 	//get the item title out and the borrower name
-	var item = req.params.item;
+	var item = req.params.item; //should be item id
 	var borrower = req.params.borrower;
 	User.find({
 		where: {
 			username: borrower
 		}
 	}).then(function(user){
-		var userId = user.id;
-		Item.find({
-			where: {
-				title: item
-			}
-		}).then(function(item){
-			req.body.itemreq_id = item.id;
+			req.body.itemreq_id = item;
 			req.body.userreq_id = user.id;
 			Notification.create(req.body)
 				.then(function(notification){
 					res.send(notification);
-				})
-		})
-	})
-}
-
-controller.getByUser = function(req, res, next){
-	var user = req.params.user;
-	User.find({
-		where: {
-			username: user
-		}
-	}).then(function(user){
-		var userId = user.id;
-		Item.findAll({
-			where: {
-				lender_id: user.id
-			}
-		}).then(function(items){
-			var itemsList = [];
-			for(var i = 0; i < items.length; i++){
-				itemsList.push(items[i].id);
-			}
-			Notification.findAll({
-				where: {
-					itemreq_id: itemsList
-				}
-			}).then(function(notifications){
-				res.json(notifications);
+				}).catch(function(error){
+					console.log('error inside of notifications create ', error);
 			})
 		})
+	}
+
+
+// controller.getByUser = function(req, res, next){
+// 	var user = req.params.user;
+// 	User.find({
+// 		where: {
+// 			username: user
+// 		}
+// 	}).then(function(user){
+// 		var userId = user.id;
+// 		Item.findAll({
+// 			where: {
+// 				lender_id: user.id
+// 			}
+// 		}).then(function(items){
+// 			var itemsList = [];
+// 			for(var i = 0; i < items.length; i++){
+// 				itemsList.push(items[i].id);
+// 			}
+// 			Notification.findAll({
+// 				where: {
+// 					itemreq_id: itemsList
+// 				}
+// 			}).then(function(notifications){
+// 				res.json(notifications);
+// 			})
+// 		})
+// 	})
+// }
+
+controller.acceptRequest = function(req, res, next){//This should delete all notifications related to the item
+	//Would this have the id of the item? I think so.
+	var borrowerId = req.params.borrower;
+	Notification.destroy({
+			where: {
+				itemreq_id: req.params.item
+			}
+		}).then(function(){
+			Item.update(
+				{borrowed: true, borrower_id: req.params.borrower},
+				{where: {id: req.params.item}}
+			)
+		}).then(function(){
+				res.send('item updated and is now borrowed         *********');
+		})
+}
+
+controller.rejectRequest = function(req, res, next){
+	//reject the request from a single user
+	Notification.destroy({
+		where: {
+			itemreq_id: req.params.item,
+			userreq_id: req.params.borrower
+		}
+	}).then(function(){
+		res.send('a particular users request for an item has been removed from the notificiations')
 	})
 }
 
